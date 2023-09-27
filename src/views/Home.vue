e7b81f7f8e34b4f6b8c94c3ac6fd4b99e5ba9e6d
Yuri Brito <103972747+yuri2bdev@users.noreply.github.com>
2023-09-26 21:33:42 -0300

    add componentes do projeto

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+<Banner />
+  <div class="main-container">
+    <h1>Monte o seu burger: </h1>
+    <BurgerForm />
+  </div>
+  </div>
+</template>
+
+<script>
+import BurgerForm from '@/components/BurgerForm.vue'
+import Banner from '../components/Banner.vue'
+export default {
+  name: "Home",
+  components: {
+    Banner,
+    BurgerForm
+  }  
+}
+</script>
